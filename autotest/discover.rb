b36c844b8a5b79ec014f5e2ff7b37c32c3dd02f8
sujimichi <sujimichi@googlemail.com>
2012-05-31 23:31:00 +0100

    removed un-needed matcher

--- autotest/discover.rb
+++ autotest/discover.rb
@@ -1,9 +1,4 @@
 Autotest.add_discovery { "rspec2" }
 #Autotest.options[:no_full_after_failed] = true
 
-Autotest.add_hook :initialize do |at|
-  at.add_mapping(%r%^lib/**/*.rb$%) {|filename, _|
-    at.files_matching filename
-  }
-end
 
